c6a1a06c7c31d6cd3892a6300acef208a1de644d
Marvin Ludersdorfer <marvin.ludersdorfer@tum.de>
2020-11-15 17:21:05 +0100

    next steps

--- file_sync/main.py
+++ file_sync/main.py
@@ -4,6 +4,9 @@ import logging
 from file_sync.src.window import Window
 
 
+# TODO: find a way to access android phone from file system
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     window = Window()
