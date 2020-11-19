84f546d3893605401c8cc190cea43960c9e6ee8b
Marvin Ludersdorfer <marvin.ludersdorfer@tum.de>
2020-11-19 16:10:38 +0100

    next steps

--- file_sync/main.py
+++ file_sync/main.py
@@ -5,10 +5,11 @@ from file_sync.src.window import Window
 
 
 # TODO: find a way to access android phone from file system
+# https://github.com/ganeshrvel/openmtp
+# https://github.com/JeffLIrion/adb_shell
+# https://github.com/google/python-adb
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     window = Window()
-
-
