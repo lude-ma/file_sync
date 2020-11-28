d46122f187ae6a7db486d017546ef0432ff6fa91
Marvin Ludersdorfer <marvin.ludersdorfer@tum.de>
2020-11-28 21:54:21 +0100

    next steps

--- file_sync/main.py
+++ file_sync/main.py
@@ -6,6 +6,9 @@ from file_sync.src.window import Window
 
 # TODO: find a way to access android phone from file system
 # https://github.com/ganeshrvel/openmtp
+
+# https://drautb.github.io/2015/07/27/the-perfect-exchange-mtp-with-python/
+
 # https://github.com/JeffLIrion/adb_shell
 # https://github.com/google/python-adb
 
